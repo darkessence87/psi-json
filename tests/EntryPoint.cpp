36019810935bf52868b4bc530ffebda583189ff5
darkessence87 <darkessence87@gmail.com>
2026-01-20 19:29:58 +0400

    [WIN] Upgrade to clang-cl compiler. Added asan + ubsan support

--- tests/EntryPoint.cpp
+++ tests/EntryPoint.cpp
@@ -7,19 +7,40 @@
 #include <gmock/gmock.h>
 #include <gtest/gtest.h>
 
+#ifdef PSI_LOGGER
+#include "psi/logger/Logger.h"
+#else
+#include <iostream>
+#include <sstream>
+#define LOG_INFO_STATIC(x)                                                                                             \
+    do {                                                                                                               \
+        std::ostringstream os;                                                                                         \
+        os << x;                                                                                                       \
+        std::cout << os.str() << std::endl;                                                                            \
+    } while (0)
+#endif
+
 using namespace ::testing;
 
 int main(int argc, char **argv)
 {
-    auto runTests = [&]() {
+    int result = 0;
+    auto runMain = [&]() {
+        LOG_INFO_STATIC("Start tests main");
+
         InitGoogleTest(&argc, argv);
-        RUN_ALL_TESTS();
+
+        result = RUN_ALL_TESTS();
+
+        LOG_INFO_STATIC("Exit tests main");
     };
 
-    runTests();
+    runMain();
 
 #ifdef _WIN32
     _CrtSetReportMode(_CRT_WARN, _CRTDBG_MODE_DEBUG);
     _CrtDumpMemoryLeaks();
 #endif
+
+    return result;
 }
